7c06574428472cba79f92c79b48c982c95b7713a
Hüseyin Kusutoğlu <kusutoglu@gmail.com>
2023-05-28 22:15:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let randomNumber1 = Math.floor(Math.random()*6+1)
-let randomDiceImage1 = `../dice${randomNumber1}.png`
+let randomDiceImage1 = `dice${randomNumber1}.png`
 
 let randomNumber2 = Math.floor(Math.random()*6+1)
-let randomDiceImage2 = `../dice${randomNumber2}.png`
+let randomDiceImage2 = `dice${randomNumber2}.png`
 
 document.querySelector(".img1").setAttribute("src",randomDiceImage1)
 document.querySelector(".img2").setAttribute("src",randomDiceImage2)
